439f161e34758d7b35d89dba3cd2b8c3a3a41295
Royfangs <royfangs@gmail.com>
2020-08-01 17:02:31 +0800

    switch knob tab and action tab for storybook.

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,8 @@ module.exports = {
   stories: ['../src/components/**/*.stories.js'],
   addons: [
     '@storybook/preset-create-react-app',
-    '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register',
+    '@storybook/addon-actions',
   ],
 };
